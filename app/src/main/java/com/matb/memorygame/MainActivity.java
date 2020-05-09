d1741b06395f65a392c835de0c7bd96257c75414
miguelteran <miguel.teranbenalcazar@mail.utoronto.ca>
2020-05-09 19:23:22 -0400

    Migrating to AndroidX

--- app/src/main/java/com/matb/memorygame/MainActivity.java
+++ app/src/main/java/com/matb/memorygame/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
 import android.os.Bundle;
